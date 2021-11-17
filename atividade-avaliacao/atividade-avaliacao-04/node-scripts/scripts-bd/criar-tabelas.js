baeea9a432c8976a8be09970dc2f7ec7d803f443
Caio <caiogrebr@gmail.com>
2021-11-17 19:53:33 -0300

    Modificando criar-tabelas.js

--- atividade-avaliacao/atividade-avaliacao-04/node-scripts/scripts-bd/criar-tabelas.js
+++ atividade-avaliacao/atividade-avaliacao-04/node-scripts/scripts-bd/criar-tabelas.js
@@ -8,24 +8,24 @@ let db = new sqlite3.Database('./node-scripts/database/DB_ATIVIDADE_04.db',(erro
     }
 })
 
+function errorInformer (error) {
+    if (error) { console.log(error); }
+}
+
 db.serialize(() => {
     //criando TB_ALUNOS
     db.run(`create table if not exists TB_ALUNOS(
         ID integer not null,
         NOME text not null,
         primary key (ID autoincrement)
-    );`, (error) => {
-        if (error) { console.log(error); }
-    });
+    );`, errorInformer);
 
     //criando TB_DISCIPLINA
     db.run(`create table if not exists TB_DISCIPLINAS(
         ID integer not null,
         NOME text not null,
         primary key (ID autoincrement)
-    );`, (error) => {
-        if (error) { console.log(error); }
-    });
+    );`, errorInformer);
 
     //criando TB_ALUNO_DISCIPLINA
     db.run(`create table if not exists TB_ALUNO_DISCIPLINA(
@@ -36,9 +36,7 @@ db.serialize(() => {
         primary key (ALUNO_ID, DISCIPLINA_ID)
         foreign key (ALUNO_ID) references TB_ALUNOS(ID)
         foreign key (DISCIPLINA_ID) references TB_DISCIPLINAS(ID)
-    );`, (error) => {
-        if (error) { console.log(error); }
-    });
+    );`, errorInformer);
 });
 
 db.close((error)=>{
