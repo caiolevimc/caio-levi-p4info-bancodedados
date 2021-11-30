b5fbceca1582f828b32c1ed72831ceefa181404c
Caio <caiogrebr@gmail.com>
2021-11-30 18:51:27 -0300

    Modificando migrations em Atividade 05

--- atividade-avaliacao/atividade-avaliacao-05/migrations/20211130151646_TB_ALUNOS.js
+++ atividade-avaliacao/atividade-avaliacao-05/migrations/20211130151646_TB_ALUNOS.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('TB_ALUNOS', table => {
         table.increments('ID').primary()
-        table.string('NOME').notNull()
+        table.text('NOME').notNull()
     });
 };
 
--- atividade-avaliacao/atividade-avaliacao-05/migrations/20211130151655_TB_DISCIPLINAS.js
+++ atividade-avaliacao/atividade-avaliacao-05/migrations/20211130151655_TB_DISCIPLINAS.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('TB_DISCIPLINAS', table => {
         table.increments('ID').primary()
-        table.string('NOME').notNull()
+        table.text('NOME').notNull()
     });
 };
 
